b25ede88513699371274769158558386d55442bd
Kazymir Rabier <kazymir.rabier@grabtaxi.com>
2022-12-28 07:28:50 +0100

    added compatibility with x86 abi

--- app/src/main/c/native-lib.c
+++ app/src/main/c/native-lib.c
@@ -101,19 +101,19 @@ static inline void parse_proc_maps_to_fetch_path(char **filepaths) {
     int fd = 0;
     char map[MAX_LINE];
     int counter = 0;
-    if ((fd = my_openat(AT_FDCWD, PROC_MAPS, O_RDONLY | O_CLOEXEC, 0)) != 0) {
+    if ((fd = openat(AT_FDCWD, PROC_MAPS, O_RDONLY | O_CLOEXEC, 0)) != 0) {
 
         while ((read_one_line(fd, map, MAX_LINE)) > 0) {
             for (int i = 0; i < NUM_LIBS; i++) {
-                if (my_strstr(map, libstocheck[i]) != NULL) {
+                if (strstr(map, libstocheck[i]) != NULL) {
                     char tmp[MAX_LENGTH] = "";
                     char path[MAX_LENGTH] = "";
                     char buf[5] = "";
                     sscanf(map, "%s %s %s %s %s %s", tmp, buf, tmp, tmp, tmp, path);
                     if (buf[2] == 'x') {
-                        size_t size = my_strlen(path) + 1;
+                        size_t size = strlen(path) + 1;
                         filepaths[i] = malloc(size);
-                        my_strlcpy(filepaths[i], path, size);
+                        strlcpy(filepaths[i], path, size);
                         counter++;
                     }
                 }
@@ -121,7 +121,7 @@ static inline void parse_proc_maps_to_fetch_path(char **filepaths) {
             if (counter == NUM_LIBS)
                 break;
         }
-        my_close(fd);
+        close(fd);
     }
 }
 
@@ -132,21 +132,21 @@ static inline bool fetch_checksum_of_library(const char *filePath, execSection *
     Elf_Shdr sectHdr;
     int fd;
     int execSectionCount = 0;
-    fd = my_openat(AT_FDCWD, filePath, O_RDONLY | O_CLOEXEC, 0);
+    fd = openat(AT_FDCWD, filePath, O_RDONLY | O_CLOEXEC, 0);
     if (fd < 0) {
         return NULL;
     }
 
-    my_read(fd, &ehdr, sizeof(Elf_Ehdr));
-    my_lseek(fd, (off_t) ehdr.e_shoff, SEEK_SET);
+    read(fd, &ehdr, sizeof(Elf_Ehdr));
+    lseek(fd, (off_t) ehdr.e_shoff, SEEK_SET);
 
     unsigned long memsize[2] = {0};
     unsigned long offset[2] = {0};
 
 
     for (int i = 0; i < ehdr.e_shnum; i++) {
-        my_memset(&sectHdr, 0, sizeof(Elf_Shdr));
-        my_read(fd, &sectHdr, sizeof(Elf_Shdr));
+        memset(&sectHdr, 0, sizeof(Elf_Shdr));
+        read(fd, &sectHdr, sizeof(Elf_Shdr));
 
 //        __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "SectionHeader[%d][%ld]", sectHdr.sh_name, sectHdr.sh_flags);
 
@@ -164,7 +164,7 @@ static inline bool fetch_checksum_of_library(const char *filePath, execSection *
     }
     if (execSectionCount == 0) {
         __android_log_print(ANDROID_LOG_WARN, APPNAME, "No executable section found. Suspicious");
-        my_close(fd);
+        close(fd);
         return false;
     }
     //This memory is not released as the checksum is checked in a thread
@@ -173,9 +173,9 @@ static inline bool fetch_checksum_of_library(const char *filePath, execSection *
     (*pTextSection)->execSectionCount = execSectionCount;
     (*pTextSection)->startAddrinMem = 0;
     for (int i = 0; i < execSectionCount; i++) {
-        my_lseek(fd, offset[i], SEEK_SET);
+        lseek(fd, offset[i], SEEK_SET);
         uint8_t *buffer = malloc(memsize[i] * sizeof(uint8_t));
-        my_read(fd, buffer, memsize[i]);
+        read(fd, buffer, memsize[i]);
         (*pTextSection)->offset[i] = offset[i];
         (*pTextSection)->memsize[i] = memsize[i];
         (*pTextSection)->checksum[i] = checksum(buffer, memsize[i]);
@@ -185,7 +185,7 @@ static inline bool fetch_checksum_of_library(const char *filePath, execSection *
 //                            memsize[i], (*pTextSection)->checksum[i]);
     }
 
-    my_close(fd);
+    close(fd);
     return true;
 }
 
@@ -198,12 +198,12 @@ void detect_frida_loop(void *pargs) {
 
     while (1) {
 
-        detect_frida_threads();
-        detect_frida_namedpipe();
+//        detect_frida_threads();
+//        detect_frida_namedpipe();
         detect_frida_memdiskcompare();
 
 
-        my_nanosleep(&timereq, NULL);
+        nanosleep(&timereq, NULL);
 
     }
 }
@@ -217,7 +217,7 @@ scan_executable_segments(char *map, execSection *pElfSectArr, const char *librar
     char tmp[100] = "";
 
     sscanf(map, "%lx-%lx %s %s %s %s %s", &start, &end, buf, tmp, tmp, tmp, path);
-    //__android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Map [%s]", map);
+    __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Map [%s]", map);
 
     if (buf[2] == 'x') {
         if (buf[0] == 'r') {
@@ -238,6 +238,8 @@ scan_executable_segments(char *map, execSection *pElfSectArr, const char *librar
                                                 pElfSectArr->memsize[i]);
 //                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Checksum:[%ld][%ld]", output,
 //                                    pElfSectArr->checksum[i]);
+                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Checksum:[%ld][%ld]", output,
+                                    pElfSectArr->checksum[i]);
 
                 if (output != pElfSectArr->checksum[i]) {
                     __android_log_print(ANDROID_LOG_VERBOSE, APPNAME,
@@ -252,13 +254,13 @@ scan_executable_segments(char *map, execSection *pElfSectArr, const char *librar
             char ch[10] = "", ch1[10] = "";
             __system_property_get("ro.build.version.release", ch);
             __system_property_get("ro.system.build.version.release", ch1);
-            int version = my_atoi(ch);
-            int version1 = my_atoi(ch1);
+            int version = atoi(ch);
+            int version1 = atoi(ch1);
             if (version < 10 || version1 < 10) {
                 __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Suspicious to get XOM in "
                                                                   "version < Android10");
             } else {
-                if (0 == my_strncmp(libraryName, LIBC, my_strlen(LIBC))) {
+                if (0 == strncmp(libraryName, LIBC, strlen(LIBC))) {
                     //If it is not readable, then most likely it is not manipulated by Frida
                     __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "LIBC Executable Section"
                                                                       " not readable! ");
@@ -285,10 +287,10 @@ static inline ssize_t read_one_line(int fd, char *buf, unsigned int max_len) {
     ssize_t ret;
     ssize_t bytes_read = 0;
 
-    my_memset(buf, 0, max_len);
+    memset(buf, 0, max_len);
 
     do {
-        ret = my_read(fd, &b, 1);
+        ret = read(fd, &b, 1);
 
         if (ret != 1) {
             if (bytes_read == 0) {
@@ -331,24 +333,24 @@ static inline void detect_frida_threads() {
         while ((entry = readdir(dir)) != NULL) {
             char filePath[MAX_LENGTH] = "";
 
-            if (0 == my_strcmp(entry->d_name, ".") || 0 == my_strcmp(entry->d_name, "..")) {
+            if (0 == strcmp(entry->d_name, ".") || 0 == strcmp(entry->d_name, "..")) {
                 continue;
             }
             snprintf(filePath, sizeof(filePath), PROC_STATUS, entry->d_name);
 
-            int fd = my_openat(AT_FDCWD, filePath, O_RDONLY | O_CLOEXEC, 0);
+            int fd = openat(AT_FDCWD, filePath, O_RDONLY | O_CLOEXEC, 0);
             if (fd != 0) {
                 char buf[MAX_LENGTH] = "";
                 read_one_line(fd, buf, MAX_LENGTH);
-                if (my_strstr(buf, FRIDA_THREAD_GUM_JS_LOOP) ||
-                    my_strstr(buf, FRIDA_THREAD_GMAIN)) {
+                if (strstr(buf, FRIDA_THREAD_GUM_JS_LOOP) ||
+                    strstr(buf, FRIDA_THREAD_GMAIN)) {
                     //Kill the thread. This freezes the app. Check if it is an anticpated behaviour
-                    //int tid = my_atoi(entry->d_name);
-                    //int ret = my_tgkill(getpid(), tid, SIGSTOP);
+                    //int tid = atoi(entry->d_name);
+                    //int ret = tgkill(getpid(), tid, SIGSTOP);
                     __android_log_print(ANDROID_LOG_WARN, APPNAME,
                                         "Frida specific thread found. Act now!!!");
                 }
-                my_close(fd);
+                close(fd);
             }
 
         }
@@ -375,7 +377,7 @@ static inline void detect_frida_namedpipe() {
             if ((filestat.st_mode & S_IFMT) == S_IFLNK) {
                 //TODO: Another way is to check if filepath belongs to a path not related to system or the app
                 my_readlinkat(AT_FDCWD, filePath, buf, MAX_LENGTH);
-                if (NULL != my_strstr(buf, FRIDA_NAMEDPIPE_LINJECTOR)) {
+                if (NULL != strstr(buf, FRIDA_NAMEDPIPE_LINJECTOR)) {
                     __android_log_print(ANDROID_LOG_WARN, APPNAME,
                                         "Frida specific named pipe found. Act now!!!");
                 }
@@ -391,11 +393,11 @@ static inline void detect_frida_memdiskcompare() {
     int fd = 0;
     char map[MAX_LINE];
 
-    if ((fd = my_openat(AT_FDCWD, PROC_MAPS, O_RDONLY | O_CLOEXEC, 0)) != 0) {
+    if ((fd = openat(AT_FDCWD, PROC_MAPS, O_RDONLY | O_CLOEXEC, 0)) != 0) {
 
         while ((read_one_line(fd, map, MAX_LINE)) > 0) {
             for (int i = 0; i < NUM_LIBS; i++) {
-                if (my_strstr(map, libstocheck[i]) != NULL) {
+                if (strstr(map, libstocheck[i]) != NULL) {
                     if (true == scan_executable_segments(map, elfSectionArr[i], libstocheck[i])) {
                         break;
                     }
@@ -407,7 +409,6 @@ static inline void detect_frida_memdiskcompare() {
                             "Error opening /proc/self/maps. That's usually a bad sign.");
 
     }
-    my_close(fd);
+    close(fd);
 
 }
-
